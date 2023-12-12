b3257c8c9e44c1900dbba84b3470c6198ec035f0
school <jtiao@westmont.edu>
2023-12-12 12:37:51 -0800

    runner implemented with shuffle

--- src/classifier/valorant_classifier/valorant_classifier_runner.py
+++ src/classifier/valorant_classifier/valorant_classifier_runner.py
@@ -11,33 +11,42 @@ __email__ = "jtiao@westmont.edu, dponcedeleon@westmont.edu"
 data = pd.read_csv('player_stats.csv')
 features = data[['player', 'map', 'kill', 'death', 'assist', 'adr', 'fk', 'fd']]
 
-# Training data
-
-training_data = data.sample(n=300, random_state=42)
-
-test_data = data.drop(training_data.index).sample(n=300, random_state=42)
-
-# Define stop words
-stop_words = set(stopwords.words('english'))
+# Determine the size of the training and test sets
+training_size = int(len(data) * 0.5)  # 70% for training
+test_size = len(data) - training_size
 
 # Create a list of training feature sets
 training_feature_sets = []
 
-for _, row in training_data.iterrows():
+  # Shuffle the data
+shuffled_indices = random.sample(range(len(data)), len(data))
+
+# Split the shuffled indices into training and test sets
+training_indices = shuffled_indices[:training_size]
+test_indices = shuffled_indices[training_size:]
+
+for idx in training_indices:
+    row = data.iloc[idx]
     known_clas = row['role']
-
     training_feature_set = ValorantFeatureSet.build(row, known_clas=known_clas)
-
     training_feature_sets.append(training_feature_set)
+
 # Train the classifier
 classifier = ValorantAbstractClassifier.train(training_feature_sets)
-
 
 # top = classifier.present_features(50)
 # print(top)
 i = 0
-for index, row in test_data.iterrows():
+correct_predictions = 0
+for idx in test_indices:
+    row = data.iloc[idx]
     test_feature_set = ValorantFeatureSet.build(row)
     predicted_role = classifier.gamma(test_feature_set)
-    print(f"The predicted role for the player in {index} is: {predicted_role}")
+    actual_role = row['role']
+    print(f"The predicted role for the player in {idx} is: {predicted_role}")
+    if predicted_role == actual_role:
+        correct_predictions += 1
     i += 1
+
+accuracy = round((correct_predictions / len(test_indices)) * 100, 2)
+print(f"Accuracy: {accuracy}%")
