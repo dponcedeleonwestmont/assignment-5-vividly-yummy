c2ba46c2efb9beef6ce2da4bac2dcb89f5439b6c
school <jtiao@westmont.edu>
2023-12-14 21:30:15 -0800

    pyspark

--- src/classifier/valorant_classifier/pyspark_runner.py
+++ src/classifier/valorant_classifier/pyspark_runner.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("Valorant").getOrCreate()
 data = spark.read.csv("../../player_stats.csv", header=True, inferSchema=True)
 
-processed_data = data.filter(data['fd'] > 5).select('number', 'player', 'map')
+processed_data = data.filter(data['fd'] > 5).select('player', 'map', 'agent')
 
 processed_data.show()
 
